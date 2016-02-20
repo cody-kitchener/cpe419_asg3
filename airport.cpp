957ae09becd6e94a3b0f9d8cb7b4133fa44bc820
cody-kitchener <codykitchener963@gmail.com>
2016-02-20 15:23:10 -0800

    added ids to Airport class

--- airport.cpp
+++ airport.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 #include "airport.h"
 
-Airport::Airport(string code, double latitude, double longitude) {
+Airport::Airport() {}
+
+Airport::Airport(string code, string id, double latitude, double longitude) {
    m_code = code;
+   m_id = id;
    m_latitude = latitude;
    m_longitude = longitude;
 }
--- airport.h
+++ airport.h
@@ -8,19 +8,24 @@ using namespace std;
 class Airport {
 private:
    string m_code;
+   string m_id;
    double m_latitude;
    double m_longitude;
 
 public:
-   Airport(string code, double lattude, double longitude);
+   Airport();
+   Airport(string code, string id, double lattude, double longitude);
 
    string getCode() {
       return m_code;
    }
+   string getID() {
+      return m_id;
+   }
    double getLat() {
       return m_latitude;
    }
-   double getLong() {
+   double getLon() {
       return m_longitude;
    }
 };
